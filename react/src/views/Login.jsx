c9b89a3b5896039692e2ed42b889442f7b4adb3e
psalmistcom <dadewale9@gmail.com>
2022-12-24 20:14:35 +0100

    login and sign endpoint consumed

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { createRef, useState } from "react";
 import { Link } from 'react-router-dom'
+import { useStateContext } from "../contexts/ContextProvider";
+import axiosClient from "../../axios-client";
 
 const Login = () => {
+    const emailRef = createRef()
+    const passwordRef = createRef()
+    const [errors, setErrors] = useState(null)
+    const { setUser, setToken } = useStateContext()
+
     const onSubmit = (ev) => {
         ev.preventDefault();
+
+        const payload = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        }
+        axiosClient.post('/login', payload)
+            .then(({ data }) => {
+                setUser(data.user)
+                setToken(data.token);
+            })
+            .catch(err => {
+                const response = err.response;
+                if (response && response.status === 422) {
+                    setErrors(response.data.errors)
+                }
+            })
     }
 
     return (
@@ -13,8 +36,15 @@ const Login = () => {
                     <h1 className="title">
                         Login to your Account
                     </h1>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
+                    {errors &&
+                        <div className="alert">
+                            {Object.keys(errors).map(key => (
+                                <p key={key}>{errors[key][0]}</p>
+                            ))}
+                        </div>
+                    }
+                    <input ref={emailRef} type="email" placeholder="Email" />
+                    <input ref={passwordRef} type="password" placeholder="Password" />
                     <button className="btn btn-block"> Login </button>
                     <p className="message">
                         Not registered? <Link to="/signup"> Create Account </Link>
--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,42 +1,49 @@
-import React, { useRef } from "react";
+import React, { createRef, useState } from "react";
 import { Link } from "react-router-dom";
 import axiosClient from "../../axios-client";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Signup = () => {
-    const nameRef = useRef();
-    const emailRef = useRef();
-    const passwordRef = useRef();
-    const passwordConfrimationRef = useRef();
+    const nameRef = createRef()
+    const emailRef = createRef()
+    const passwordRef = createRef()
+    const passwordConfirmationRef = createRef()
+    const { setUser, setToken } = useStateContext()
+    const [errors, setErrors] = useState(null)
 
-    const { setUser, setToken } = useStateContext();
+    const onSubmit = ev => {
+        ev.preventDefault()
 
-    const onSubmit = (ev) => {
-        ev.preventDefault();
         const payload = {
             name: nameRef.current.value,
             email: emailRef.current.value,
             password: passwordRef.current.value,
-            password_confrimation: passwordConfrimationRef.current.value,
-        };
-        axiosClient.post("/signup", payload)
+            password_confirmation: passwordConfirmationRef.current.value,
+        }
+        axiosClient.post('/signup', payload)
             .then(({ data }) => {
-                setUser(data.user);
+                setUser(data.user)
                 setToken(data.token);
             })
             .catch(err => {
-                const response = err.response
+                const response = err.response;
                 if (response && response.status === 422) {
-                    console.log(response.data.errors)
+                    setErrors(response.data.errors)
                 }
             })
-            ;
-    };
+    }
     return (
         <div className="login-signup-form animated fadeInDown">
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="title"> Create a new Account </h1>
+                    {errors &&
+                        <div className="alert">
+                            {Object.keys(errors).map(key => (
+                                <p key={key}>{errors[key][0]}</p>
+                            ))}
+                        </div>
+                    }
                     <input ref={nameRef} type="text" placeholder="Full Name" />
                     <input
                         ref={emailRef}
@@ -49,7 +56,7 @@ const Signup = () => {
                         placeholder="Password"
                     />
                     <input
-                        ref={passwordConfrimationRef}
+                        ref={passwordConfirmationRef}
                         type="password"
                         placeholder="Password Confirmation"
                     />
