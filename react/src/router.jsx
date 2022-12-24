af0e0f72b1ac027bbf7c0e1c576e3b3ea7af2465
psalmistcom <dadewale9@gmail.com>
2022-12-24 10:28:17 +0100

    fixed issues

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom'
-import DefaultLayout from './components/DefaultLayout'
-import GuestLayout from './components/GuestLayout'
-import Dashboard from './views/Dashboard'
-import Login from './views/Login'
-import NotFound from './views/NotFound'
-import Signup from './views/signup'
-import Users from './views/users'
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import Dashboard from "./views/Dashboard";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
+import Login from "./views/Login";
+import NotFound from "./views/NotFound";
+import Signup from "./views/Signup";
+import Users from "./views/Users";
 
 const router = createBrowserRouter([
     {
@@ -17,13 +17,13 @@ const router = createBrowserRouter([
                 element: <Navigate to="/users" />,
             },
             {
+                path: '/dashboard',
+                element: <Dashboard />
+            },
+            {
                 path: '/users',
                 element: <Users />
             },
-            {
-                path: '/dashboard',
-                element: <Dashboard />
-            }
         ]
     },
     {
