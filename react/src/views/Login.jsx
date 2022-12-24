560e52ee9bbe296f1b41fa9c9627f011903fae23
psalmistcom <dadewale9@gmail.com>
2022-12-24 11:09:05 +0100

    added signup and axios

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
 const Login = () => {
     const onSubmit = (ev) => {
         ev.preventDefault();
@@ -9,10 +10,13 @@ const Login = () => {
         <div className="login-signup-form animated fadeInDown">
             <div className="form">
                 <form onSubmit={onSubmit}>
+                    <h1 className="title">
+                        Login to your Account
+                    </h1>
                     <input type="email" placeholder="Email" />
                     <input type="password" placeholder="Password" />
                     <button className="btn btn-block"> Login </button>
-                    <p>
+                    <p className="message">
                         Not registered? <Link to="/signup"> Create Account </Link>
                     </p>
                 </form>
