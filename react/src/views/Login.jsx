7877b150975882cf029f5cf7dfd82051ca480071
psalmistcom <dadewale9@gmail.com>
2022-12-24 10:43:54 +0100

    login page

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Login = () => {
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+    }
+
     return (
-        <div>Login</div>
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <button className="btn btn-block"> Login </button>
+                    <p>
+                        Not registered? <Link to="/signup"> Create Account </Link>
+                    </p>
+                </form>
+            </div>
+        </div>
     )
 }
 
